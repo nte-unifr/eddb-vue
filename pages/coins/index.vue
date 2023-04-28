594aefeb85cc996b9a97afb5ea28d3fd9df595c4
Alrick Deillon <alrick.deillon@unifr.ch>
2023-04-28 14:24:00 +0200

    Add simple pagination

--- pages/coins/index.vue
+++ pages/coins/index.vue
@@ -1,5 +1,21 @@
 <script setup>
-const { data, pending, error } = await useAsyncGql('GetCoins')
+const currentPage = ref(1)
+const coins = ref([])
+
+watch(currentPage, () => {
+  fetchData()
+})
+
+const fetchData = async () => {
+  const { data, pending, error } = await useAsyncGql('GetCoins', { page: currentPage })
+  coins.value = data?.value?.coins || []
+}
+
+function setPage(pageNumber) {
+  currentPage.value = pageNumber
+}
+
+fetchData()
 </script>
 
 <template>
@@ -7,8 +23,8 @@ const { data, pending, error } = await useAsyncGql('GetCoins')
     <div class="drawer drawer-mobile">
       <input id="main-drawer" type="checkbox" class="drawer-toggle" />
       <div class="drawer-content flex flex-col pt-24 pb-14">
-        <CoinsStats :count="data?.coins.length" :total="data?.coins.length" />
-        <div v-if="data?.coins" class="overflow-x-auto w-full px-8">
+        <CoinsStats :count="coins.length" :total="coins.length" />
+        <div v-if="coins" class="w-full px-8">
           <table class="table w-full">
             <thead>
               <tr>
@@ -16,7 +32,7 @@ const { data, pending, error } = await useAsyncGql('GetCoins')
               </tr>
             </thead>
             <tbody>
-              <CoinsRow v-for="coin in data.coins" :coin="coin" />
+              <CoinsRow v-for="coin in coins" :coin="coin" />
             </tbody>
             <tfoot>
               <tr>
@@ -28,7 +44,13 @@ const { data, pending, error } = await useAsyncGql('GetCoins')
       </div>
       <div class="drawer-side">
         <label for="main-drawer" class="drawer-overlay"></label>
-        <div class="menu p-4 w-80 bg-base-200 text-base-content py-28 prose"><Filters /></div>
+        <div class="menu p-4 w-80 bg-base-200 text-base-content py-28 prose">
+          <Filters />
+          <button @click="setPage(1)">Page 1</button>
+          <button @click="setPage(2)">Page 2</button>
+          <input v-model="page" type="text" placeholder="1" />
+          Current page : {{ page }}
+        </div>
       </div>
     </div>
   </div>

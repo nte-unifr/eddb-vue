6f797ca2ab75d1e82991c9df6f65d62305fc4a3f
Alrick Deillon <alrick.deillon@unifr.ch>
2023-07-07 16:26:32 +0200

    Add a gototop on page changes

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@ const store = useSettingsStore()
 </script>
 
 <template>
-  <div :data-theme="store.theme" class="bg-base-300">
+  <div id="app" :data-theme="store.theme" class="bg-base-300">
     <Navbar />
     <div class="flex justify-center min-h-screen pt-24 px-8 mb-16">
       <NuxtPage />
--- components/Table/TablePager.vue
+++ components/Table/TablePager.vue
@@ -3,7 +3,7 @@ const collectionStore = useCollectionStore()
 const store = usePagerStore()
 
 function scrollToTop() {
-  const element = document.getElementById('nuxtpage');
+  const element = document.getElementById('app');
   if (element) {
     element.scrollIntoView();
   }
@@ -12,15 +12,16 @@ function scrollToTop() {
 
 <template>
   <div class="flex items-center">
+    {{ y }}
     <p class="mr-4 hidden lg:block"><strong>{{ store.total }}</strong>&nbsp;résultats sur&nbsp;<strong>{{ collectionStore.total }}</strong></p>
     <div class="btn-group">
-      <button @click="store.prev()" class="btn btn-sm btn-ghost" :class="{ 'btn-disabled': store.page == 1 }">
+      <button @click="scrollToTop(); store.prev();" class="btn btn-sm btn-ghost" :class="{ 'btn-disabled': store.page == 1 }">
         <IconPrev />
       </button>
       <template v-for="p in store.max">
-        <button @click="store.goTo(p)" class="btn btn-sm btn-ghost" :class="{ 'btn-active': p == store.page }">{{ p }}</button>
+        <button @click="scrollToTop(); store.goTo(p)" class="btn btn-sm btn-ghost" :class="{ 'btn-active': p == store.page }">{{ p }}</button>
       </template>
-      <button @click="store.next()" class="btn btn-sm btn-ghost" :class="{ 'btn-disabled': store.page == store.max }">
+      <button @click="scrollToTop(); store.next()" class="btn btn-sm btn-ghost" :class="{ 'btn-disabled': store.page == store.max }">
         <IconNext />
       </button>
     </div>

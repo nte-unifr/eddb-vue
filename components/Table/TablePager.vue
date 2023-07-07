ca969bfbf4af70231dee4f9f4e7d297d5b287a97
Alrick Deillon <alrick.deillon@unifr.ch>
2023-07-07 16:28:38 +0200

    Typo

--- components/Table/TablePager.vue
+++ components/Table/TablePager.vue
@@ -12,7 +12,6 @@ function scrollToTop() {
 
 <template>
   <div class="flex items-center">
-    {{ y }}
     <p class="mr-4 hidden lg:block"><strong>{{ store.total }}</strong>&nbsp;résultats sur&nbsp;<strong>{{ collectionStore.total }}</strong></p>
     <div class="btn-group">
       <button @click="scrollToTop(); store.prev();" class="btn btn-sm btn-ghost" :class="{ 'btn-disabled': store.page == 1 }">

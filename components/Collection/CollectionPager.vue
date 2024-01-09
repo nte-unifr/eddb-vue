0bbe0da580d20a1dd479d4649c0a1b67dda15266
Alrick Deillon <alrick.deillon@unifr.ch>
2024-01-09 11:44:12 +0100

    Fix scroll error with pagination

--- components/Collection/CollectionPager.vue
+++ components/Collection/CollectionPager.vue
@@ -10,12 +10,11 @@ const firstPage = computed(() => page.value === 1)
 const lastPage = computed(() => page.value === maxPage.value)
 
 watchEffect(() => {
-  if(page.value < 1) {
+  if (typeof page.value != "number" || page.value < 1) {
     page.value = 1
   } else if (page.value > maxPage.value) {
     page.value = maxPage.value
   }
-  scrollToTop()
 })
 </script>
 
@@ -24,10 +23,10 @@ watchEffect(() => {
     <p class="mr-4 hidden lg:block">
       <strong>{{ filterCount }}</strong> résultats sur <strong>{{ totalCount }}</strong>
     </p>
-    <button class="btn btn-sm mx-2" @click="page--" :disabled="firstPage"><IconPrev /></button>
     <div class="mx-2">Page</div>
-    <input v-model="page" @focus="($event.target as HTMLInputElement).select()" type="text" class="input input-bordered input-md input-primary text-center font-bold max-w-14 mx-2" />
+    <input v-model="page" @focus="($event.target as HTMLInputElement).select()" type="text" class="input input-bordered input-md input-primary text-center font-bold max-w-14" />
     <div class="font-bold mx-2">/ {{ maxPage }}</div>
-    <button class="btn btn-sm mx-2" @click="page++" :disabled="lastPage"><IconNext /></button>
+    <button class="btn btn-sm ml-2" @click="page--;scrollToTop()" :disabled="firstPage"><IconPrev /></button>
+    <button class="btn btn-sm" @click="page++;scrollToTop()" :disabled="lastPage"><IconNext /></button>
   </div>
 </template>

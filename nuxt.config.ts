f19abe0ceeb76c8ed5b44d73bb3137914c519132
Rehan Mulakhel <rehan.mulakhel@unifr.ch>
2024-02-20 17:35:35 +0100

    Add files related to Docker.
    Note: will only work with project `nafo` for now.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  "app": {
+    "baseURL": "/nafo/",
+    "buildAssetsDir": "/_nuxt/",
+    "cdnURL": ""
+  },
   imports: {
     dirs: ['utils/directus']
   },
